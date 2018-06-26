1f2905f470dc97a19f7598b5882c6de3d89d8d7e
Sivan Mehta <Sivanmehta2@gmail.com>
2018-06-25 19:40:24 -0700

    add time estimates

--- promises.js
+++ promises.js
@@ -5,15 +5,25 @@ async function sleep(ms) {
 async function complete(i) {
   await sleep(Math.random() * 10 * 1000);
 
-  console.log(i);
+  const soFar = ++completed; // iteration
+  const left = limit - soFar; // iterations
+  var duration = (Date.now() - started) / 1000; // time
+  const pace = duration / soFar; // seconds per iteration
+  var eta = pace * left // seconds;
+
+  eta = Math.round(eta * 1000) / 1000;
+  duration = Math.round(duration * 1000) / 1000;
+
+  console.log(`${soFar},${eta},${duration}`);
 }
 
 async function run() {
-  const promises = [...Array(1000).keys()].map(complete);
-  await Promise.all(promises)
+  const promises = [...Array(limit).keys()].map(complete);
+  await Promise.all(promises);
 }
 
-const limit = 1000;
+const limit = 10000;
 let completed = 0;
+const started = new Date();
 
-run()
+run();
